605447bdacaaa949c728cae2bec96c114d2ec587
Kaveh Karimadini <112331207+kavehkarimadini@users.noreply.github.com>
2024-03-15 06:20:35 -0700

    Update NER_Dashboard.py

--- NER_Dashboard.py
+++ NER_Dashboard.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import streamlit as st
-import plotly.graph_objects as go
+# import plotly.graph_objects as go
 import plotly.express as px
 from plotly.subplots import make_subplots
 
@@ -25,30 +25,54 @@ def load_data():
     print("res_cols: ",res_cols[-1],type(res_cols[-1]))
     final = result.rename(columns={res_cols[-1]:"Frequency"})
     print(final.columns.tolist())
-    return final
+    return final[final["Frequency"]>3]
 
-def plotly_func(df,label,x_col,y_col):
+def filter_labels(F_df,label):
+    return F_df[F_df[target_col]==label]
+
+def plotly_bar_func(df,labels,label,x_col,y_col):
     # freq = freq.Country.value_counts().reset_index().rename(columns={"index": "x"})
 
     # read in 3d volcano surface data
     # df_v = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/volcano.csv")
 
     # Initialize figure with subplots
-    fig = make_subplots(
-        rows=2, cols=2,
-        column_widths=[0.6, 0.4],
-        row_heights=[0.4, 0.6],
-        specs=[[{"type": "bar", "rowspan": 2}, {"type": "bar"}],
-            [            None                    , {"type": "surface"}]])
+    # fig = make_subplots(
+    #     rows=2, cols=2,
+    #     # column_widths=[0.6, 0.4],
+    #     # row_heights=[0.4, 0.6],
+    #     subplot_titles=['Bar Chart', 'Pie Chart'],
+    #     specs=[[{"type": 'bar', "colspan": 2},None],[{"type": 'pie', "colspan": 2},None]])#, {"type": "bar"}],[            None                    , {"type": "surface"}]])
 
     # Add scattergeo globe map of volcano locations
-    fig.add_trace(
-        go.bar(df, x=x_col, y=y_col,
-             hover_data=['count'], color='count',
-             labels={x_col:'Named Entities',y_col:"frequency"}, height=400),
-        row=1, col=1
-    )
-
+    if label not in labels:
+        # fig.add_trace(
+        #     go.Bar(x=df[x_col],y=df[y_col], marker=dict(color="crimson"), showlegend=False),
+        #     row=1, col=1
+        # )
+        # fig.add_trace(
+        #     go.Pie(labels=df[x_col],values=df[y_col], name='Pie Chart'),
+        #     row=2, col=1
+        # )
+        # Create a bar chart
+        fig = px.bar(df, x=x_col, y=y_col, color=df["labels"],
+                    title='Frequency of Text Elements',
+                    labels={'count':'Frequency', 'text':'Text Elements'},
+                    template='plotly_dark')
+    else:
+        filtered_df = filter_labels(df,label)
+        # fig.add_trace(
+        #     go.Bar(x=filtered_df[x_col],y=filtered_df[y_col], marker=dict(color="crimson"), showlegend=False),
+        #     row=1, col=1
+        # )
+        # fig.add_trace(
+        #     go.Pie(labels=filtered_df[x_col],values=filtered_df[y_col], name='Pie Chart'),
+        #     row=2, col=1
+        # )
+        fig = px.bar(filtered_df, x=x_col, y=y_col, color=filtered_df["labels"],
+            title='Frequency of Text Elements',
+            labels={'count':'Frequency', 'text':'Text Elements'},
+            template='plotly_dark')
     # # Add locations bar chart
     # fig.add_trace(
     #     go.Bar(x=freq["x"][0:10],y=freq["Country"][0:10], marker=dict(color="crimson"), showlegend=False),
@@ -71,21 +95,13 @@ def plotly_func(df,label,x_col,y_col):
     # )
 
     # Rotate x-axis labels
-    fig.update_xaxes(tickangle=45)
-
-    # Set theme, margin, and annotation in layout
+    # fig.update_xaxes(tickangle=45)
+    # Customize layout
     fig.update_layout(
-        template="plotly_dark",
-        margin=dict(r=10, t=25, b=40, l=60),
-        annotations=[
-            dict(
-                text="Source: NOAA",
-                showarrow=False,
-                xref="paper",
-                yref="paper",
-                x=0,
-                y=0)
-        ]
+        xaxis_title='Text Elements',
+        yaxis_title='Frequency',
+        legend_title='Labels',
+        font=dict(family="Courier New, monospace", size=18, color="white")
     )
     return fig
 # Boolean to resize the dataframe, stored as a session state variable
@@ -110,15 +126,13 @@ label = st.selectbox(
  "What do you want the x variable to be?",
  labels,index=None,placeholder="choose a label"
 )
-def filter_labels(F_df,label):
-    return F_df[F_df[target_col]==label]
 if label in labels:
     filtered_df = filter_labels(df,label)
     edited_df = st.data_editor(filtered_df, num_rows="dynamic",use_container_width=st.session_state.use_container_width)
 else:
     edited_df = st.data_editor(df, num_rows="dynamic",use_container_width=st.session_state.use_container_width)
-
-# st.plotly_chart()
+plt_fig = plotly_bar_func(df,labels,label,columns[0],columns[-1])
+st.plotly_chart(plt_fig)
 
 # favorite_command = edited_df.loc[edited_df["rating"].idxmax()]["command"]
 # st.markdown(f"Your favorite command is **{favorite_command}** 🎈")
